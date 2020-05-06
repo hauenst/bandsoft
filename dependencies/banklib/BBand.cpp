16f2b786ac513e354eca677fb9c57e6fe5432b38
hauenst <hauenst@jlab.org>
2020-05-06 19:00:55 -0400

    * Modified BBand class to be compatible with old and new files. Overloaded constructor. Getter functions should be backward compatibel in scripts

--- dependencies/banklib/BBand.cpp
+++ dependencies/banklib/BBand.cpp
@@ -1,12 +1,21 @@
 #include "BBand.h"
 #include "TVector3.h"
 // ==============================================================
-BBand::BBand(hipo::schema __schema) : hipo::bank(__schema){
+//Constructor for old hipo files, overloaded with extra input integer
+BBand::BBand(hipo::schema __schema, int __old) : hipo::bank(__schema){
+
+  bool oldfile = true;
 
 	id_order           = __schema.getEntryOrder("id"          );
 	sector_order       = __schema.getEntryOrder("sector"      );
 	layer_order        = __schema.getEntryOrder("layer"       );
 	component_order    = __schema.getEntryOrder("component"   );
+	x_order            = __schema.getEntryOrder("x"           );
+	y_order            = __schema.getEntryOrder("y"           );
+	z_order            = __schema.getEntryOrder("z"           );
+	ux_order           = __schema.getEntryOrder("ux"          );
+	uy_order           = __schema.getEntryOrder("uy"          );
+	uz_order           = __schema.getEntryOrder("uz"          );
 	meantimeTdc_order  = __schema.getEntryOrder("meantimeTdc" );
 	meantimeFadc_order = __schema.getEntryOrder("meantimeFadc");
 	difftimeTdc_order  = __schema.getEntryOrder("difftimeTdc" );
@@ -17,14 +26,44 @@ BBand::BBand(hipo::schema __schema) : hipo::bank(__schema){
 	tFadcRcorr_order   = __schema.getEntryOrder("tFadcRcorr"  );
 	tTdcLcorr_order    = __schema.getEntryOrder("tTdcLcorr"   );
 	tTdcRcorr_order    = __schema.getEntryOrder("tTdcRcorr"   );
+	energy_order       = -1;
+	indexLpmt_order	   = -1;
+	indexRpmt_order    = -1;
+	status_order			 = -1;
+
+}
+//STANDARD constructor for new files
+BBand::BBand(hipo::schema __schema) : hipo::bank(__schema){
+
+  bool oldfile = false;
+
+	id_order           = __schema.getEntryOrder("id"          );
+	sector_order       = __schema.getEntryOrder("sector"      );
+	layer_order        = __schema.getEntryOrder("layer"       );
+	component_order    = __schema.getEntryOrder("component"   );
 	x_order            = __schema.getEntryOrder("x"           );
 	y_order            = __schema.getEntryOrder("y"           );
 	z_order            = __schema.getEntryOrder("z"           );
-	ux_order           = __schema.getEntryOrder("ux"          );
-	uy_order           = __schema.getEntryOrder("uy"          );
-	uz_order           = __schema.getEntryOrder("uz"          );
+	ux_order           = __schema.getEntryOrder("ex"          );
+	uy_order           = __schema.getEntryOrder("ey"          );
+	uz_order           = __schema.getEntryOrder("ez"          );
+	meantimeTdc_order  = __schema.getEntryOrder("time"        );
+	meantimeFadc_order = __schema.getEntryOrder("timeFadc"    );
+	difftimeTdc_order  = __schema.getEntryOrder("difftime"    );
+	difftimeFadc_order = __schema.getEntryOrder("difftimeFadc");
+	energy_order       = __schema.getEntryOrder("energy"      );
+	indexLpmt_order	   = __schema.getEntryOrder("indexLpmt"   );
+	indexRpmt_order    = __schema.getEntryOrder("indexRpmt"   );
+	status_order			 = __schema.getEntryOrder("status"      );
+	adcLcorr_order     = -1;
+	adcRcorr_order     = -1;
+	tFadcLcorr_order   = -1;
+	tFadcRcorr_order   = -1;
+	tTdcLcorr_order    = -1;
+	tTdcRcorr_order    = -1;
 
 }
+
 // ==============================================================
 BBand::~BBand(){}
 // ==============================================================
--- dependencies/include/BBand.h
+++ dependencies/include/BBand.h
@@ -16,36 +16,46 @@ class BBand : public hipo::bank {
 
 	private:
 
+    bool oldfile ;
+		//for both BAND::hits (old and new structure)
 		int id_order           ;
 		int sector_order       ;
 		int layer_order        ;
 		int component_order    ;
-		int meantimeTdc_order  ;
-		int meantimeFadc_order ;
-		int difftimeTdc_order  ;
-		int difftimeFadc_order ;
+		int x_order            ;
+		int y_order            ;
+		int z_order            ;
+		int ux_order           ; //corresponds to ex in new BAND::hits
+		int uy_order           ; //corresponds to ey in new BAND::hits
+		int uz_order           ; //corresponds to ez in new BAND::hits
+		int meantimeTdc_order  ; //corresponds to time in new BAND::hits
+		int meantimeFadc_order ; //corresponds to timeFadc in new BAND::hits
+		int difftimeTdc_order  ; //corresponds to difftime in new BAND::hits
+		int difftimeFadc_order ; //corresponds to difftimeFadc in new BAND::hits
+		//only new BAND::hits structure
+		int energy_order       ;
+		int indexLpmt_order    ;
+		int indexLpmt_order    ;
+		int status_order       ;
+		//only old BAND::hits structure
 		int adcLcorr_order     ;
 		int adcRcorr_order     ;
 		int tFadcLcorr_order   ;
 		int tFadcRcorr_order   ;
 		int tTdcLcorr_order    ;
 		int tTdcRcorr_order    ;
-		int x_order            ;
-		int y_order            ;
-		int z_order            ;
-		int ux_order           ;
-		int uy_order           ;
-		int uz_order           ;
+
 
 	public:
 
 		BBand(){};
 
+		BBand(hipo::schema __schema,int __old);
 		BBand(hipo::schema __schema);
 
 		~BBand();
 
-
+		bool  isOldfile       () 					{ return oldfile;}
 		int   getId           (int index) { return getInt   (id_order           ,index);}
 		int   getSector       (int index) { return getInt   (sector_order       ,index);}
 		int   getLayer        (int index) { return getInt   (layer_order        ,index);}
@@ -66,6 +76,17 @@ class BBand : public hipo::bank {
 		float getUx           (int index) { return getFloat (ux_order           ,index);}
 		float getUy           (int index) { return getFloat (uy_order           ,index);}
 		float getUz           (int index) { return getFloat (uz_order           ,index);}
+		//next functions are for BAND::hits new structure. some of them are mapped to the same class members than previous get-functions
+		float getEx           (int index) { return getFloat (ux_order           ,index);}
+		float getEy           (int index) { return getFloat (uy_order           ,index);}
+		float getEz           (int index) { return getFloat (uz_order           ,index);}
+		float getTime         (int index) { return getFloat (meantimeTdc_order  ,index);}
+		float getTimeFadc     (int index) { return getFloat (meantimeFadc_order ,index);}
+		float getEnergy       (int index) { return getFloat (energy_order       ,index);}
+		int   getLpmtindex    (int index) { return getInt   (indexLpmt_order    ,index);}
+		int   getRpmtindex    (int index) { return getInt   (indexRpmt_order    ,index);}
+		int   getStatus       (int index) { return getInt   (status_order       ,index);}
+
 
 		int   getBarKey(int index);
 };
